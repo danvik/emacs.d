a093eb4ab86846be65557b25c1ac519deb02ce97
danvik <odvikstrom@gmail.com>
2018-03-27 20:19:50 +0200

    use goimports as formatter

--- init.el
+++ init.el
@@ -596,6 +596,7 @@ using `fit-window-to-buffer'."
               ("C-c C-e" . go-gopath-set-gopath)
               ("C-c C-r" . go-remove-unused-imports))
   :config
+  (setq gofmt-command "goimports")
   (add-hook 'before-save-hook #'gofmt-before-save)
   (add-hook 'go-mode-hook (lambda ()
                             (set (make-local-variable 'company-backends) '(company-go))
