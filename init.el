2858b772195e6810f8f7ccf5d0fbe5dd1c1d3aac
danvik <odvikstrom@gmail.com>
2019-12-15 23:20:15 +0100

    stop saving when idle

--- init.el
+++ init.el
@@ -638,12 +638,11 @@ Inserted by installing org-mode or when a release is made."
 (use-package super-save
   :straight t
   :config
-  (setq super-save-auto-save-when-idle t
+  (setq super-save-auto-save-when-idle nil
         super-save-exclude '(".gpg"))
   (setq auto-save-list-file-prefix nil
         auto-save-default nil
-        make-backup-files nil
-        super-save-idle-duration 30)
+        make-backup-files nil)
   (add-to-list 'super-save-triggers 'ace-window)
   :init
   (super-save-mode +1))
