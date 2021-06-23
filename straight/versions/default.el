6d8f3816c5ba814b0522a843ccc0a7135badd9c3
Daniel Vikström <odvikstrom@gmail.com>
2021-06-23 19:01:57 +0200

    freeze

--- straight/versions/default.el
+++ straight/versions/default.el
@@ -1,6 +1,8 @@
-(("emacsmirror-mirror" . "1c24bceb43e0e475656be908133a8eb6df3bf973")
+(("el-get" . "d76ac84ae9670de7bf7725b362cafe86688771f9")
+ ("epkgs" . "ffe236e983b1006e4e91ec141e5e3d3bc2440d23")
  ("gnu-elpa-mirror" . "3e7e5862acc80a6371fae2e1bf087442402f7c54")
- ("melpa" . "d4ababc787d4dd173c65cc1b4b4a0fc0bb6e6d07")
+ ("melpa" . "1bebe80350df731711bc526ca9fe942410211220")
+ ("no-littering" . "57357e15643158b4e0d9b3b4f70a82f5fc73178a")
  ("straight.el" . "e1390a933b6f5a15079d6dec91eac97a17aad10c")
  ("use-package" . "a7422fb8ab1baee19adb2717b5b47b9c3812a84c"))
-:alpha
+:beta
