011e34bda1618d6d84823d408e09a36df2af511b
danvik <odvikstrom@gmail.com>
2022-10-04 19:51:53 +0200

    rm ctrlf update themes

--- init.el
+++ init.el
@@ -504,12 +504,7 @@
           ("b" "Bookmark (Clipboard)" entry (file+headline org-bookmarks-file "new")
            "** %(org-web-tools--org-link-for-url)\n:PROPERTIES:\n:TIMESTAMP: %t\n:END:%?\n" :empty-lines 1 :prepend t))))
 
-;;; search and grep
-
-(use-package ctrlf
-  :straight t
-  :init
-  (ctrlf-mode +1))
+;;; grep
 
 (use-package deadgrep
   :straight t
@@ -647,7 +642,7 @@
            nord-theme
            solarized-theme
            doom-themes
-           modus-themes))
+           ef-themes))
   (straight-use-package package))
 
 (load-theme 'modus-operandi t)
