e14e9e9421ba048233e77e7a61c65f2aebed580c
danvik <odvikstrom@gmail.com>
2024-09-04 19:58:26 +0200

    update straight installation

--- init.el
+++ init.el
@@ -7,31 +7,24 @@
 
 ;;; bootstrap `straight-use-package'
 
-(progn
-  (setq straight-repository-branch "develop")
-  (defvar bootstrap-version)
-  (let ((bootstrap-file
-         (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-        (bootstrap-version 6))
-    (unless (file-exists-p bootstrap-file)
-      (with-current-buffer
-          (url-retrieve-synchronously
-           "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-           'silent 'inhibit-cookies)
-        (goto-char (point-max))
-        (eval-print-last-sexp)))
-    (load bootstrap-file nil 'nomessage))
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
-  ;; https://github.com/raxod502/straight.el/issues/700
-  (setq straight-recipes-emacsmirror-use-mirror nil)
-
-  (if (fboundp 'use-package)
-      (add-to-list 'straight-built-in-pseudo-packages 'use-package)
-    (straight-use-package 'use-package))
-
-  (setq use-package-verbose 't)
-
-  (add-to-list 'straight-built-in-pseudo-packages 'project))
+(add-to-list 'straight-built-in-pseudo-packages 'project)
+(setq use-package-enable-imenu-support t)
 
 ;;; local settings and variables
 
