c6873b35663d8e0ffefe219b3574bda239102f7c
danvik <odvikstrom@gmail.com>
2024-09-07 21:40:18 +0200

    collect dired

--- init.el
+++ init.el
@@ -194,27 +194,27 @@
 ;;; dired
 
 (use-package dired
-  :hook (dired-mode . dired-hide-details-mode))
+  :hook (dired-mode . dired-hide-details-mode)
+  :init
+  (use-package dired-subtree
+    :straight t
+    :after dired
+    :bind (:map dired-mode-map
+                ("i" . dired-subtree-insert)
+                (";" . dired-subtree-remove)
+                ("<tab>" . dired-subtree-toggle)
+                ("<backtab>" . dired-subtree-cycle))
+    :config
+    (setq dired-subtree-use-backgrounds nil))
 
-(use-package dired-subtree
-  :straight t
-  :after dired
-  :bind (:map dired-mode-map
-              ("i" . dired-subtree-insert)
-              (";" . dired-subtree-remove)
-              ("<tab>" . dired-subtree-toggle)
-              ("<backtab>" . dired-subtree-cycle))
-  :config
-  (setq dired-subtree-use-backgrounds nil))
+  (use-package dired-filter
+    :straight t
+    :after dired)
 
-(use-package dired-filter
-  :straight t
-  :after dired)
-
-(use-package dired-narrow
-  :straight t
-  :after dired
-  :bind (:map dired-mode-map ("\\" . dired-narrow)))
+  (use-package dired-narrow
+    :straight t
+    :after dired
+    :bind (:map dired-mode-map ("\\" . dired-narrow))))
 
 ;;; files
 
